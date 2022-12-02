733dee2834ae8c4f44b2cd34b3eb846106285fe8
DESKTOP-E0NP7K0\Anna <marcinfurmanczyk@wp.pl>
2022-12-02 19:35:27 +0100

    Add pitch and reservations

---
+++ API/Entities/Pitch.cs
@@ -0,0 +1,13 @@
+﻿namespace API.Entities
+{
+    public class Pitch
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }    
+        public string Adress { get; set; }
+        public int CoordLat { get; set; }
+        public int CoordLon { get; set; }
+        public int MaxPlayers { get; set; }
+        public List<Reservation> Reservations { get; set; }
+    }
+}

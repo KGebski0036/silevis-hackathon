2dbbd20b2205690e3e8765dc0fa3e00297bd3d3e
DESKTOP-E0NP7K0\Anna <marcinfurmanczyk@wp.pl>
2022-12-02 22:31:00 +0100

    added UserRepository

---
+++ API/Data/UserRepository.cs
@@ -0,0 +1,43 @@
+﻿using API.Entities;
+using API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using SQLitePCL;
+using System.Reflection.Metadata.Ecma335;
+
+namespace API.Data
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DataContext _context;
+
+        public UserRepository(DataContext context)
+        {
+            _context=context;
+        }
+
+        public async Task<IEnumerable<AppUser>> GetAllUsersAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<AppUser> GetUserByIdAsync(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task<AppUser> GetUserByUserNameAsync(string username)
+        {
+            return await _context.Users.SingleOrDefaultAsync(x => x.UserName == username);
+        }
+
+        public async Task<bool> SaveAllAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public void Update(AppUser user)
+        {
+            _context.Entry(user).State= EntityState.Modified;
+        }
+    }
+}

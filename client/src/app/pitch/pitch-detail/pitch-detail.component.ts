c100907aa5736136d9290f59f9b66c3de2d13214
Jacek Jarosz <jacekjarosz55@gmail.com>
2022-12-03 10:35:39 +0100

    geforce gtx msi za darmo original taste slonecznych przechowywac w chlodnym

--- client/src/app/pitch/pitch-detail/pitch-detail.component.ts
+++ client/src/app/pitch/pitch-detail/pitch-detail.component.ts
@@ -1,15 +1,14 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute, ParamMap, Router, RouteReuseStrategy } from '@angular/router';
+import { Router } from '@angular/router';
 import { GameEvent } from 'src/app/_models/event';
 import { Pitch } from 'src/app/_models/pitch';
-import { PitchService } from 'src/app/_services/pitch.service';
 
 @Component({
   selector: 'app-pitch-detail',
   templateUrl: './pitch-detail.component.html',
   styleUrls: ['./pitch-detail.component.css']
 })
-export class PitchDetailComponent {
+export class PitchDetailComponent implements OnInit{
 
   @Input()
   pitch?: Pitch;
@@ -17,11 +16,14 @@ export class PitchDetailComponent {
   @Input()
   events: GameEvent[] = [];
 
+
   addEventClicked(pitchId: number) {
     this.router.navigateByUrl('/events/add/' + pitchId);
   }
 
   constructor(private router: Router) {
+  }
+  ngOnInit(): void {
   }
 
 

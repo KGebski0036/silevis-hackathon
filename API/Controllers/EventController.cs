ee429dc6ec90a88fdad76b8f4bd614daff6e6877
DESKTOP-E0NP7K0\Anna <marcinfurmanczyk@wp.pl>
2022-12-03 03:16:26 +0100

    add event controller

---
+++ API/Controllers/EventController.cs
@@ -0,0 +1,39 @@
+﻿using API.Data;
+using API.DTOs;
+using API.Entities;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace API.Controllers
+{
+    public class EventController : BaseApiController
+    {
+        private readonly DataContext _context;
+        private readonly IMapper _mapper;
+        public EventController(DataContext context, IMapper mapper)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Event>>> GetEvents()
+        {
+            var Events = await _context.Events.ToListAsync();
+
+            return Events;
+        }
+        [HttpGet("{id}")]
+
+        public async Task<ActionResult<EventDto>> GetEvent(int eventId)
+        {
+            return await _context.Events
+                .Where(p => p.Id == eventId)
+                .ProjectTo<EventDto>(_mapper.ConfigurationProvider).SingleOrDefaultAsync();
+        }
+    }
+}
---
+++ API/DTOs/EventDto.cs
@@ -0,0 +1,15 @@
+﻿using API.Entities;
+
+namespace API.DTOs
+{
+    public class EventDTO
+    {
+        public int Id { get; set; }
+        public int PinId { get; set; }
+        public Pitch Pitch { get; set; }
+        public List<AppUser> Participants { get; set; }
+        public int MaxPlayers { get; set; }
+        public DateTime DateFrom { get; set; }
+        public DateTime DateTo { get; set; }
+    }
+}
